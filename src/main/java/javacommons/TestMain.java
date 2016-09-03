ce6540cc063450494939b8be391a28d8d934bf95
root <root@root-pc>
2016-09-04 03:51:48 +0900

    .

---
+++ src/main/java/javacommons/TestMain.java
@@ -0,0 +1,11 @@
+package javacommons;
+
+public class TestMain {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int sum = javacommons.util.Utils.add2(11, 22);
+        System.out.println(sum);
+    }
+}
---
+++ src/main/java/javacommons/util/Utils.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javacommons.util;
+
+/**
+ *
+ * @author root
+ */
+public class Utils {
+    /**
+     * Add 2 integers
+     * @param x
+     * @param y
+     * @return sum of x and y
+     */
+    public static int add2(int x, int y)
+    {
+        return x + y;
+    }
+}
